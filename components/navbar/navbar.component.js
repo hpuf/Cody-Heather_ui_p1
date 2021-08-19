6bd188a0ce39dab42a5c180de4f3b9b5cbaabd6b
hpuf <csmcdn@gmail.com>
2021-08-18 20:30:00 -0500

    missed a couple details, navbar works as expected now

--- components/navbar/navbar.component.js
+++ components/navbar/navbar.component.js
@@ -4,7 +4,7 @@ const NAVBAR_ELEMENT = document.getElementById('navbar');
 function NavbarComponent(){
 
     let template = `
-    <nav id="navbar-component" class="navbar navbar-dark bg-dark" aria-label="navbar-component">
+    <nav id="navbar-component" class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="navbar-component">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">NCU</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,17 +17,14 @@ function NavbarComponent(){
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Logout</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a id="logout" class="nav-link" href="#">Logout</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
                         <ul class="dropdown-menu" aria-labelledby="dropdown01">
-                            <li><a class="dropdown-item" href="#">Login</a></li>
-                            <li><a class="dropdown-item" href="#">Register</a></li>
-                            <li><a class="dropdown-item" href="#">Dashboard</a></li>
+                            <li><a id="nav-to-login" class="dropdown-item" data-route="/login">Login</a></li>
+                            <li><a id="nav-to-register" class="dropdown-item" data-route="/register">Register</a></li>
+                            <li><a id="nav-to-dashboard" class="dropdown-item" data-route="/dashboard">Dashboard</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -40,7 +37,7 @@ function NavbarComponent(){
     
     `;
 
-    function injectTemplate(){
+    function injectTemplate(cb){
         NAVBAR_ELEMENT.innerHTML = template;
         cb();
     }
@@ -53,7 +50,7 @@ function NavbarComponent(){
     }
 
     this.render = function() {
-        // Can reference to this function because it's within scope.
+        // Can reference to this function because it is within scope.
         injectTemplate( () => {
             document.getElementById('logout').addEventListener('click', logout);
             document.getElementById('nav-to-login').addEventListener('click', navigateToView);
