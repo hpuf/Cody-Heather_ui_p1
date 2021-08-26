0896046adafcc246ac95a8005d8f4dca9615976f
hpuf <csmcdn@gmail.com>
2021-08-25 22:48:25 -0500

    woops forgot to save env back

--- util/env.js
+++ util/env.js
@@ -2,5 +2,5 @@ const APP_ROOT = document.getElementById('app-root');
 
 export default{
     rootDiv: APP_ROOT,
-    apiUrl: 'http://localhost:8080/'
+    apiUrl: 'http://codyheatherp1-env.eba-2b7mpkdp.us-east-2.elasticbeanstalk.com'
 }
